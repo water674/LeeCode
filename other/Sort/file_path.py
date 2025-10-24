a9b850708bea38759f94529b67ced3f6f8d14527
Zergen.X <129514587+water674@users.noreply.github.com>
2025-10-24 19:07:41 +0800

    Update file_path.py

--- other/Sort/file_path.py
+++ other/Sort/file_path.py
@@ -1,7 +1,7 @@
 import pickle
 
 # 修改路径为自己文件夹即可
-FILE_PATH = r'C:\Users\xuzhou123\Desktop\排序\generated_data.pkl'
+FILE_PATH = r'..\generated_data.pkl'
 
 def load_nums():
     with open(FILE_PATH, 'rb') as f:
@@ -10,3 +10,4 @@ def load_nums():
 
 def file_path():
     return FILE_PATH
+
